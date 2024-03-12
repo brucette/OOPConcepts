0739478d9387b7f9ca210bf6d3200fe421bc434d
Tina Bruce <tina@fishface.se>
2024-03-12 12:09:47 +0100

    More polymorphism

--- OOPConcepts/Animal.cs
+++ OOPConcepts/Animal.cs
@@ -5,19 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-// 3.3:
-// fråga 13 - om samtliga fåglar skulle behöva ett nytt attribut, skulle det läggas till klass Bird.
-// fråga 14 - om alla djur skulle behöva ett nytt attribut, skulle det läggas till klass Animal.
+// **** PUNKT 3.3 **** 
+
+// FRÅGA 13 - om samtliga fåglar skulle behöva ett nytt attribut, skulle det läggas till klass Bird.
+// FRÅGA 14 - om alla djur skulle behöva ett nytt attribut, skulle det läggas till klass Animal.
 
 namespace OOPConcepts
 {
     internal abstract class Animal
     {
         public string Name { get; set; }
-        public int Weight { get; set; }
+        public double Weight { get; set; }
         public int Age { get; set; }
 
-        public Animal(string name, int weight, int age)
+        public Animal(string name, double weight, int age)
         {
             Name = name;
             Weight = weight;
@@ -28,98 +29,123 @@ namespace OOPConcepts
 
         public virtual string Stats()
         {
-            return GetType().Name;
+            return $"Properties for {GetType().Name}" +
+                $"\nName: {Name}" +
+                $"\nWeight: {Weight}" +
+                $"\nAge: {Age}\n";
         }
     }
 
     // Inheriting from Animal class:
     internal class Horse : Animal
     {
-        public string Height { get; set; }
-
-        public Horse(string name, int weight, int age, string height) : base(name, weight, age)
+        public int Height { get; set; }
+        public Horse(string name, double weight, int age, int height) : base(name, weight, age)
         {
             Height = height;
         }
-
         public override void DoSound()
         {
-            Console.WriteLine($"{GetType().Name} says Iiihahahaaa");
+            Console.WriteLine("iiihahahaaa");
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"Height: {Height}\n";
         }
     }
 
     internal class Dog: Animal 
     {
         public string Breed { get; set; }
-
-        public Dog(string name, int weight, int age, string breed) : base(name, weight, age)
+        public Dog(string name, double weight, int age, string breed) : base(name, weight, age)
         {
             Breed = breed;
         }
-
         public override void DoSound()
         {
-            Console.WriteLine($"{GetType().Name} says Woof woof");
+            Console.WriteLine("woof woof");
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"Breed: {Breed}\n";
+        }
+        public string CatchBall()
+        {
+            return "bounce bounce run run";
         }
     }
 
     internal class Hedgehog: Animal
     { 
         public int NrOfSpikes { get; set; }
-
-        public Hedgehog(string name, int weight, int age, int nrOfSpikes) : base(name, weight, age)
+        public Hedgehog(string name, double weight, int age, int nrOfSpikes) : base(name, weight, age)
         {
                 NrOfSpikes = nrOfSpikes;
         }
-
         public override void DoSound()
         {
-            Console.WriteLine($"{GetType().Name} says Sniff sniff");
+            Console.WriteLine("sniff sniff");
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"NrOfSpikes: {NrOfSpikes}\n";
         }
     }
 
     internal class Worm : Animal 
     {
         public bool IsPoisonous { get; set; }
-
-        public Worm(string name, int weight, int age, bool isPoisonous) : base(name, weight, age)
+        public Worm(string name, double weight, int age, bool isPoisonous) : base(name, weight, age)
         {
             IsPoisonous = isPoisonous;
         }
-
         public override void DoSound()
         {
-            Console.WriteLine($"{GetType().Name} says Sssssssssss");
+            Console.WriteLine("ssssssssss");
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"IsPoisonous: {IsPoisonous}\n";
         }
     }
 
     internal class Bird : Animal 
     { 
         public int WingSpan { get; set; }
-
-        public Bird(string name, int weight, int age, int wingSpan) : base(name, weight, age)
+        public Bird(string name, double weight, int age, int wingSpan) : base(name, weight, age)
         {
             WingSpan = wingSpan;
         }
-
         public override void DoSound()
         {
-            Console.WriteLine($"{GetType().Name} says Kraahkraah");
+            Console.WriteLine("kraahkraah");
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"WingSpan: {WingSpan}\n";
         }
     }
 
     internal class Wolf : Animal 
     {
         public string Color { get; set; }
-
-        public Wolf(string name, int weight, int age, string color) : base(name, weight, age)
+        public Wolf(string name, double weight, int age, string color) : base(name, weight, age)
         {
             Color = color;
         }
-
         public override void DoSound()
         {
-            Console.WriteLine($"{GetType().Name} says Auououououououuuuuu");
+            Console.WriteLine("auououououououuuuuu");
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"Color: {Color}\n";
         }
     }
 
@@ -127,30 +153,42 @@ namespace OOPConcepts
     internal class Pelican: Bird 
     {
         public int BeakLength { get; set; }
-
-        public Pelican(string name, int weight, int age, int wingSpan, int beakLength) : base(name, weight, age, wingSpan)
+        public Pelican(string name, double weight, int age, int wingSpan, int beakLength) : base(name, weight, age, wingSpan)
         {
             BeakLength = beakLength;
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"BeakLength: {BeakLength}\n";
         }
     }
 
     internal class Flamingo : Bird 
     {
         public int LegLength { get; set; }
-
-        public Flamingo(string name, int weight, int age, int wingSpan, int legLength) : base(name, weight, age, wingSpan)
+        public Flamingo(string name, double weight, int age, int wingSpan, int legLength) : base(name, weight, age, wingSpan)
         {
             LegLength = legLength;
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"LegLength: {LegLength}\n";
         }
     }
 
     internal class Swan : Bird 
     {
         public int NeckLength { get; set; }
-
-        public Swan(string name, int weight, int age, int wingSpan, int neckLength) : base(name, weight, age, wingSpan)
+        public Swan(string name, double weight, int age, int wingSpan, int neckLength) : base(name, weight, age, wingSpan)
         {
             NeckLength = neckLength;
+        }
+        public override string Stats()
+        {
+            string result = base.Stats();
+            return result + $"NeckLength: {NeckLength}\n";
         }
     }
 
@@ -162,10 +200,8 @@ namespace OOPConcepts
 
     internal class Wolfman: Wolf, IPerson 
     {
-        public Wolfman(string name, int weight, int age, string color) : base(name, weight, age, color)
-        {
-           
-        }
+        public Wolfman(string name, double weight, int age, string color) : base(name, weight, age, color)
+        { }
 
         public void Talk() 
         {
--- OOPConcepts/Program.cs
+++ OOPConcepts/Program.cs
@@ -9,19 +9,22 @@ namespace OOPConcepts
             try
             {
                 // **** PUNKT 3.1.1 ****
-                // Person mathias = new Person(38, "Mathias", "Wahl", 1.70, 75);
+
+                Person mathias = new Person(38, "Mathias", "Wahl", 1.70, 75);
                 // Console.WriteLine($"{mathias.age} {mathias.name} {mathias.lName} {mathias.fName} {mathias.weight} {mathias.height}"); // all private fields are inaccessible
-                
-                // Console.WriteLine($"Age is: {mathias.Age}"); // accessible
-                // mathias.Age = -3;
 
                 // All the following throw the correct error messages:
-                // Person mathias2 = new Person(0, "Mathias", "Wahl", 1.70, 75);
-                // Person mathias3 = new Person(38, "Mathiasmath", "Wahl", 1.70, 75);
-                // Person mathias4 = new Person(38, "Mathias", "Wa", 1.70, 75);
-                // Person mathias = new Person(38, "", "Wa", 1.70, 75);
+                // mathias.Age = -3;
+                // mathias.FName = "";
+                // mathias.FName = "M";
+                // mathias.FName = "Mathiasmath";
+                // mathias.LName = "";
+                // mathias.LName = "Wa";
+                // mathias.LName = "Wahlwahlwahlwahl";
+
 
                 // **** PUNKT 3.1.5 ****
+
                 PersonHandler personHandler = new PersonHandler();
 
                 Person chris = personHandler.CreatePerson(18, "Chris", "Ronaldo", 1.70, 75);
@@ -52,7 +55,9 @@ namespace OOPConcepts
                 Console.ResetColor();
             }
 
+
             // **** PUNKT 3.2.7 - 8 ****
+
             UserError error1 = new TextInputError();
             UserError error2 = new NumericInputError();
             UserError error3 = new NumericInputError();
@@ -71,9 +76,87 @@ namespace OOPConcepts
                 Console.WriteLine(error.UEMessage());
             }
 
-            Wolfman wolf = new Wolfman("thewolf", 34, 34, "grey");
-            wolf.DoSound();
-            wolf.Talk();
+
+            // **** PUNKT 3.4.3 ****
+
+            Horse horse = new Horse("Johnny", 200, 12, 2);
+            Dog dog = new Dog("Beet", 70, 7, "great dane");
+            Hedgehog hedgehog = new Hedgehog("Spiky", 5, 5, 167);
+            Worm worm = new Worm("Squiggles", 0.2, 1, false);
+            Wolf wolf = new Wolf("Luna", 90, 4, "white");
+            Bird bird = new Bird("Tweeter", 0.5, 2, 20);
+            Pelican pelican = new Pelican("Mouthy", 20, 8, 90, 30);
+            Swan swan = new Swan("Necky", 30, 1, 120, 30);
+            Flamingo flamingo = new Flamingo("Pinky", 20, 9, 60, 80);
+            Wolfman wolfman = new Wolfman("Adam", 150, 100, "grey");
+            Dog eje = new Dog("Eje", 30, 3, "french bulldog");
+            Dog latte = new Dog("Latte", 40, 6, "finnish lapphund");
+
+
+            List<Animal> animals = new List<Animal>
+            {
+                horse, dog, hedgehog, worm, wolf, bird, pelican, swan, flamingo, wolfman, eje, latte   
+            };
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine($"\nAnimal type is: {animal.GetType().Name}");
+                animal.DoSound();
+
+                if (animal is IPerson)
+                { 
+                    IPerson animalPerson = (IPerson) animal;
+                    animalPerson.Talk();
+                }
+            }
+            Console.WriteLine();
+
+            // **** PUNKT 3.4.8 ****
+
+            List<Dog> dogs = new List<Dog>();
+
+
+            /* FRÅGA 9 & 10
+               dogs.Add(horse); - detta fungerar inte eftersom listan tar bara djur som är av typ Dog,
+               då Horse inte ärver Dog, funkar det inte. Alla djur ärver antingen direkt eller indirekt från Animal,
+               listan måste därför vara av typ Animal för att alla klasser ska kunna lagras tillsammans. 
+            */
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.Stats());
+            }
+
+            /* FRÅGA 13
+               för varje djur i listan kallas det just den typens Stats() metod. 
+               Varje Stats() metod anropar basklassens metod, vilket returnerar namn, vikt och ålder,      
+               egenskaperna som finns i varenda klass. Sedan läggs till de eventuella specifika 
+               egenskaperna som finns i subklassen och returnernas alltihop. 
+            */
+
+
+            // **** PUNKT 3.4.14 ****
+
+            Console.WriteLine("PROPERTIES FOR INSTANCES OF DOGS");
+
+            foreach (Animal animal in animals)
+            {
+                if (animal is Dog)
+                { 
+                    Console.WriteLine(animal.Stats());
+                    // animal.CatchBall();
+                    Dog castDog = (Dog) animal;
+                    Console.WriteLine(castDog.CatchBall());
+                }
+            }
+
+            /* FRÅGA 16 & 17
+               Man kommer inte åt metod CatchBall() från Animal listan då metoden inte 
+               finns i Animal klass. Listan innehåller instancer/objekt som behandlas av typ Animal 
+               som bara har tillgång till Animals metoder.
+               Genom att type-casta varje instans av Dog i Animal listan till Dog inom loopen, 
+               kan man sen komma åt metoder även i Dog klassen.
+            */
         }
     }
 }
